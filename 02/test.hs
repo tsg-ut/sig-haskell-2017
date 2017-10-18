a3a18ca917f2e88e85bc7973c6934349a96ed358
Yuki Yamashita <yyama0704@gmail.com>
2017-10-18 18:40:39 +0900

    remove test

--- 02/test.hs
+++
@@ -1,9 +0,0 @@
-bmiTell weight height
-  | bmi <= skinny = "痩せ"
-  | bmi <= normal = "普通"
-  | bmi <= fat = "太りぎみ"
-  | otherwise = "かなり太ってる"
-  where bmi = weight / height ^ 2
-        skinny = 18.5
-        normal = 25.0
-        fat = 30.0
